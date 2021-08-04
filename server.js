ae992436a95e8127c9859533e4207a3314a43e74
bayan-hmaidy98 <bayanhdy98@gmail.com>
2021-08-05 01:24:35 +0300

    backend for lab08

--- server.js
+++ server.js
@@ -1,9 +1,13 @@
 
 const express = require('express') // require the express package
-const weatherData = require('./data/weather.json')
 require('dotenv').config();
-const PORT = process.env.PORT
+const PORT = process.env.PORT;
+const WEATHERBIT_KEY = process.env.WEATHERBIT_KEY;
+const WEATHERBIT_URL = process.env.WEATHERBIT_URL;
+const MOVIESDB_KEY = process.env.MOVIESDB_KEY;
+const MOVIESDB_URL = process.env.MOVIESDB_URL;
 const cors = require('cors');
+const axios = require("axios");
 const server = express() // initialize your express app instance
 server.use(cors())// a server endpoint 
 server.get('/', // our endpoint name
@@ -13,38 +17,46 @@ server.get('/', // our endpoint name
     )
   });
 
-class Forecast {
-  constructor(element) {
-    this.valid_date = `${element.valid_date}`;
-    this.description = `${element.weather.description}`;
-  }
-}
-
-server.get('/weather', (req, res) => {
-  try {
-    let searchQuery = req.query.searchQuery;
-    let lon = req.query.lon;
-    let lat = req.query.lat;
-
-    const weather = weatherData.find(item => item.city_name.toLowerCase() === searchQuery.toLowerCase() || (item.lon === lon && item.lat === lat)
-      // lowercase is required in order to allow the user to enter the word in lowercase or uppercase
-    );
+try {
+  server.get('/weather', async (req, res) => {
+    const { lon, lat } = req.query;
+    const queryParameters = {
+      query: {
+        key: WEATHERBIT_KEY,
+        lon: lon,
+        lat: lat,
+      }
+    }
+    const weather = await axios.get(WEATHERBIT_URL, queryParameters);
     const weatherDataArr = weather.data.map(item =>
-      // let valid_date = item.valid_date;
-      // let description = `Low of ${item.low_temp} , high of ${item.high_temp} with ${item.weather.description} `;
+     
       new Forecast(item));
     res.send(weatherDataArr);
+  });
 
+  server.get('/movies', async (req, res) => {
+   const searchQuery = req.query.searchQuery;
+   const page = 1
+    const queryParameters = {
+      query: {
+        searchQuery: searchQuery,
+        key: MOVIESDB_KEY,
+        page: page,
+      }
+    }
+    const movies = await axios.get(MOVIESDB_URL, queryParameters);
+    const moviesDataArr = movies.data.map(item =>
+      new BestMovies(item));
+    res.send(moviesDataArr);
+ 
+  });
 
-  }
-  catch (error) {
-    res.send('Error! Please enter a valid city');
-    console.log(weatherData);
-  }
-
-});
-
-
+}
+catch (error) {
+  res.send('Error! Please enter a valid city');
+  console.log(weatherData);
+};
 server.listen(PORT, () => {
   console.log(`Server started on ${PORT}`);
+
 });
