e995882b6d385b7855e755d346beb0971494d07d
bayan-hmaidy98 <bayanhdy98@gmail.com>
2021-08-14 22:33:21 +0300

    finish lab10 without the stretch goal

--- controller/Movies.controller.js
+++ controller/Movies.controller.js
@@ -3,6 +3,8 @@ const { query } = require('express');
 const MOVIESDB_KEY = process.env.MOVIESDB_KEY;
 const MOVIESDB_URL = process.env.MOVIESDB_URL;
 const Movies = require('../BestMovies')
+const Cashe = require('../helper/cache.helper')
+let casheObj = new Cashe();
 
 
 const getMovies = async (req, res) => {
@@ -20,8 +22,36 @@ const getMovies = async (req, res) => {
     }).catch(error => res.send({ message: error.message }));
 
    
+ casheObj.movies.push({
+    'query': query,
+    'moviesList': moviesList
+})
+
 
 };
+const getMovieFromCache = async (req,res) => {
+    let {query} = req.query;
+// We need to reset the cache if one day passed 
+if ((Date.now() - casheObj.timeStamp) > 86400000){
+casheObj = new Cashe;
+}
+
+// We need to check if the cache is empty or not 
+if (casheObj.movies.length){
+    const filterdMovie = casheObj.movies.find (movie => {
+        return movie.query === query
+    })
+    if (filterdMovie){
+        res.send(filterdMovie.data)
+    }
+    else {
+        res.send(await getMovies(query))
+    }
+}
+else {
+    res.send(await getMovies(query))
+}
+}
 
 
 module.exports = getMovies;
