54f265eabacfd10e2051cdeabd68cf8ebb5e33e6
bayan-hmaidy98 <bayanhdy98@gmail.com>
2021-08-14 14:54:27 +0300

    finish lab09

--- server.js
+++ server.js
@@ -2,14 +2,12 @@
 const express = require('express') // require the express package
 require('dotenv').config();
 const PORT = process.env.PORT;
-const WEATHERBIT_KEY = process.env.WEATHERBIT_KEY;
-const WEATHERBIT_URL = process.env.WEATHERBIT_URL;
-const MOVIESDB_KEY = process.env.MOVIESDB_KEY;
-const MOVIESDB_URL = process.env.MOVIESDB_URL;
 const cors = require('cors');
 const axios = require("axios");
 const server = express() // initialize your express app instance
 server.use(cors())// a server endpoint 
+
+
 server.get('/', // our endpoint name
   (request, response) => {
     // callback function of what we should do with our request
@@ -17,49 +15,14 @@ server.get('/', // our endpoint name
     )
   });
 
+const getWeather = require('./controller/Forecast.controller')
+const getMovies = require('./controller/Movies.controller');
 
-  server.get('/weather', async (req, res) => {
-    const { lon, lat } = req.query;
-    const queryParameters = {
-      query: {
-        key: WEATHERBIT_KEY,
-        lon: lon,
-        lat: lat,
-      }
-    }
-    const weather = await axios.get(WEATHERBIT_URL, queryParameters);
-    const weatherDataArr = weather.data.map(item =>
-     
-      new Forecast(item));
-    // res.send(weatherDataArr);
-  });
+server.get('/weather', getWeather);
 
-  server.get('/movies', async (req, res) => {
-   const searchQuery = req.query.searchQuery;
+server.get('/movies', getMovies);
 
-   const movies = `${MOVIESDB_URL}?api_key=${MOVIESDB_KEY}&query=${searchQuery}&page=1]`;
-   axios.get(movies).then(response => {
-    let  moviesRes = response.data.results;
 
-      let moviesList = moviesRes.map(element => {
-        return new Movies(element);
-      });
-      res.send(moviesList);
-
-    }).catch(error=>res.send({message:error.message}));
-  });
-
-//     const moviesDataArr = movies.data.map(item =>
-//       new BestMovies(item));
-//     res.send(moviesDataArr);
- 
-//   });
-
-// }
-// catch (error) {
-//   res.send('Error! Please enter a valid city');
-//   console.log(weatherData);
-// };
 server.listen(PORT, () => {
   console.log(`Server started on ${PORT}`);
 
